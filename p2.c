957bed8829874f68e4db9a1fbbd0d3a1a2c40ebe
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-21 17:18:23 +0000

    practtice

---
+++ p2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int input_side()
+{
+  int n;
+  printf("enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int check_scalence(int a,int b,int c)
+{
+  if(a>b || a>c)
+  {
+    
+  }
+  
+}
