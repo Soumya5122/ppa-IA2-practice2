bc1624ea2f48d1b2e895b709a9b15ebc9f311345
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-21 18:31:23 +0000

    practice

--- p2.c
+++ p2.c
@@ -9,9 +9,25 @@ int input_side()
 
 int check_scalence(int a,int b,int c)
 {
-  if(a>b || a>c)
+  if(a!=b && b!=c && c!=a)
   {
-    
-  }
+    printf("is not a triangle");
   
 }
+  else
+  {
+    printf("is a triangle");
+  }
+void output(int a,int b,int c,int isscalence)
+{
+  printf("the sides are not equal %d\n",a,b,c,isscalence);
+}
+
+  int main()
+  {
+    int a,b,c,isscalence;
+    input(a,b,c);
+    check_scalence(a,b,c);
+    output(a,b,c,isscalence);
+    return 0;
+    }
